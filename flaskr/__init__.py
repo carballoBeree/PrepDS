af517813284f2d26b30fcf915aab100eba1c9bf8
Adriana Muruchi <carballobere2856@gmail.com>
2024-05-21 11:28:09 -0300

    blog , auth

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -42,5 +42,13 @@ def create_app():
 
     from . import auth
     app.register_blueprint(auth.bp)
-    
+
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
